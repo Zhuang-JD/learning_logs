3386ea70dcc172c6b5a1a54350d65917b493d737
Zhuang-JD <921506533@qq.com>
2020-09-04 18:15:59 +0800

    first commit

---
+++ learning_logs/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Topic(models.Model):
+	'''User Learning Topics'''
+	text = models.CharField(max_length=200)
+	date_added = models.DateTimeField(auto_now_add = True)
+	owner = models.ForeignKey(User,on_delete=models.CASCADE)
+	
+	def __str__(self):
+		'''return models's str'''
+		return self.text
+	
+class Entry(models.Model):
+	#xue dao de you guan mou ge zhu ti de ju ti zhi shi 
+	topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+	
+	class Meta:
+		verbose_name_plural = 'entries'
+		
+	def __str__(self):
+		#return models's str
+		return self.text[:50] + '...'		
---
+++ test/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\资料\桌面\资料整合\learning_log\test\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
